e113a5476e2cf0f48d9c7e335bece057795e432c
BigBlackBob-93 <101857203+BigBlackBob-93@users.noreply.github.com>
2022-09-28 20:52:31 +0700

    задание

---
+++ public/source/task.cpp
@@ -0,0 +1,7 @@
+// this file you need to fill
+// этот файл вам нужно заполнить
+#include "task.h"
+
+void Task::checkIntersections(const std::vector<Triangle> &in_triangles, std::vector<int> &out_count)
+{
+}
